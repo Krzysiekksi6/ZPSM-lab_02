79e07bfe9c2ee865d50f0f1149ecb9028f876a94
Krzysiek <krzysiekksi6@gmail.com>
2022-10-14 10:28:22 +0200

    feature min and max

--- app.js
+++ app.js
@@ -7,4 +7,15 @@ function avg(items) {
     return _.mean(items)
 }
 
+/** zad 2 */
+function max(items) {
+    return _.max(items)
+}
+
+function  min(items) {
+    return _.min(items)
+}
+
 console.log(avg(testArray))
+console.log(max(testArray));
+console.log(min(testArray));
